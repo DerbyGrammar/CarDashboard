87d98fedfbfbe1f4d92d59028e9204f166421e8f
Chris <c.nethercott@icloud.com>
2016-10-07 18:54:42 +0100

    Just something to show it is a proof of concept

--- src/CarDashboard/CarDashboard.ino
+++ src/CarDashboard/CarDashboard.ino
@@ -4,11 +4,11 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  
+  lcdPrint(random(1,24), random(1,32), random(1,99), random(1,99), random(1,35), temp > 30 ? true : false);
 }
 
 void lcdPrint(int volt, int amp, int temp, int temp2, int mphspeed, bool isFanOn) {
